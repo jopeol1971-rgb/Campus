b812a8e707904b7e2caf5c517933f343ca26c30b
Joaquin <jopeol1971@gmail.com>
2026-02-02 13:14:56 +0100

    lunes 2/2

--- hola.py
+++ hola.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template    
+from flask import Flask, render_template, request   
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+        usuario = request.form["user"]
+        password = request.form["password"]
+        email = request.form["email"]
+        color = request.form["color"]
+
+        print("Usuario ingresado:", usuario)
+        print("Contraseña ingresada:", password)
+        return render_template("user.html", usuario=usuario, email=email, color=color)
+    
     return render_template("login.html")
+
+
+
